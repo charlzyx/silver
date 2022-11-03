b97068d40f038ab34596bb3c4eaef582d7d17f25
xiaochao.yang <xiaochao.yang@relxtech.com>
2022-11-03 20:51:36 +0800

    feat: 🎸 simple and navi

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+
+#[tokio::main]
+async fn main() {
+    let path = std::env::args().nth(1).unwrap_or(".".into());
+    let port = std::env::args()
+        .nth(2)
+        .unwrap_or("23333".into())
+        .parse::<u16>()
+        .unwrap_or(23333_u16);
+    println!(
+        "Static File Http Server Ver 0.0.1\nRoot Dir: {}\nUsage: httpSvr [root_dir_default_.] [port_default_23333]\nhttp://127.0.0.1:{}\n",
+        path,port
+    );
+    let api = warp::fs::dir(path);
+    let server = warp::serve(api);
+    server.run(([0, 0, 0, 0], port)).await;
+}
